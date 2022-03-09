a7b5e1c9c1f6153657080a5959a2781a891a039a
skskdmsdl <skskdmsdl@gmail.com>
2022-03-09 23:17:10 +0900

    lambda study

--- src/main/java/lambdasinaction/MyLambda.java
+++ src/main/java/lambdasinaction/MyLambda.java
@@ -1,5 +1,8 @@
 package lambdasinaction;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 public class MyLambda {
     public static void main(String[] args) {
         // Thread 생성
@@ -21,5 +24,27 @@ public class MyLambda {
         
         // ctrl + shift + f10 실행 단축키
         // 이클립스는 ctrl + f11
+
+        List<String> myList = List.of("Java", "Python", "Scalar");
+        for (String value : myList){
+            System.out.println("value = " + value);
+        }
+
+        // forEach의 argument 타입은 Consumer
+        // void accept(T t)
+        // 1. 익명 내부 클래스(Anonymous Inner Class)
+        myList.forEach((new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("s = " + s);
+            }
+        }));
+
+        // 2. Lambda Expression
+        myList.forEach(val -> System.out.println(val));
+
+        //3. Method Reference
+        myList.forEach(System.out::println);
+
     }
 }
