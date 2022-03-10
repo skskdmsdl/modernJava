0eab1e5e68de4e2e92f941d9bd776c22df389883
skskdmsdl <skskdmsdl@gmail.com>
2022-03-10 16:48:38 +0900

    FilteringApples update

--- src/main/java/lambdasinaction/_01lambda/basic2/FilteringApples.java
+++ src/main/java/lambdasinaction/_01lambda/basic2/FilteringApples.java
@@ -9,23 +9,32 @@ public class FilteringApples{
 
         List<Apple> inventory = Arrays.asList(new Apple(80,"green"),
                 new Apple(155, "green"),
-                new Apple(120, "red"));
+                new Apple(120, "red"),
+                new Apple(160, "brown"));
 
+        Predicate<Apple> greenApple = apple -> apple.getColor().equals("green");
         // 람다식 사용 [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
+        filterApples(inventory, apple -> apple.getColor().equals("green")).forEach(System.out::println);
 
-
+        System.out.println("---- GreenHeavyApples 람다식");
         // 람다식 사용[Apple{color='green', weight=155}]
+        Predicate<Apple> greenHeavyApples = greenApple.and(apple -> apple.getWeight() > 150);
+        filterApples(inventory, greenHeavyApples).forEach(apple -> System.out.println("apple = " + apple));
 
-
+        System.out.println("---- GreenHeavyApples Method Reference");
         // Method Reference 사용 [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
-
+        filterApples(inventory, greenHeavyApples).forEach(System.out::println);
 
         // Method Reference 사용 [Apple{color='green', weight=155}]
 
 
         // []
-        List<Apple> weirdApples = filterApples(inventory, (Apple a) -> a.getWeight() < 80 ||
-                "brown".equals(a.getColor()));
+        Predicate<Apple> lightApple = apple -> apple.getWeight() < 150;
+        Predicate<Apple> brownOrLightApple = lightApple.or(apple -> apple.getColor().equals("brown"));
+
+        /* List<Apple> weirdApples = filterApples(inventory, (Apple a) -> a.getWeight() < 150  ||
+                "brown".equals(a.getColor()));*/
+        List<Apple> weirdApples = filterApples(inventory, brownOrLightApple);
         System.out.println(weirdApples);
     }
 
@@ -38,8 +47,10 @@ public class FilteringApples{
         return true;
     }
 
-    public static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p){
+    public static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> predicate){
+        List<Apple> appleList = new ArrayList<>();
+        inventory.forEach((apple -> {if(predicate.test(apple)) appleList.add(apple);}));
 
-        return null;
+        return appleList;
     }
 }
