780dc3b442c6218945a41155ecfa5da1f97e975c
skskdmsdl <skskdmsdl@gmail.com>
2022-03-11 22:07:40 +0900

    Operator update

--- src/main/java/lambdasinaction/_01lambda/functional/operator/OperatorExample.java
+++ src/main/java/lambdasinaction/_01lambda/functional/operator/OperatorExample.java
@@ -13,10 +13,22 @@ public class OperatorExample {
 		return result; }
 
 	public static void main(String[] args) {
+		// alt + shift + l(엘) : 이클립스 리턴타입과 변수 자동생성 // new ArrayList<String>();
+		// ctrl + alt + v : idea 리턴타입과 변수 자동생성
 		// 최대값 얻기
-		
-		
+//		maxOrMin((n1, n2) -> {if (n1 >= n2) return n1; else return n2;});
+		int max = maxOrMin((n1, n2) -> {
+			if (n1 >= n2) return n1;
+			else return n2;
+		});
+		System.out.println("max = " + max);
+
 		// 최소값 얻기
-		
+		int min = maxOrMin((a, b) -> {
+			if (a <= b) return a;
+			else return b;
+		});
+		System.out.println("min = " + min);
+
 	}
 }
