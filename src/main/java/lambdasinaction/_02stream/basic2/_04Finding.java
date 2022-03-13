d3bde97bf613d0a86b87c4a5890a57dfc01dcfb9
skskdmsdl <skskdmsdl@gmail.com>
2022-03-13 15:18:19 +0900

    terminal operation

--- src/main/java/lambdasinaction/_02stream/basic2/_04Finding.java
+++ src/main/java/lambdasinaction/_02stream/basic2/_04Finding.java
@@ -1,9 +1,9 @@
 package lambdasinaction._02stream.basic2;
-import lambdasinaction._02stream.basic1.*;
 
-import java.util.*;
+import lambdasinaction._02stream.basic1.Dish;
 
-import static lambdasinaction._02stream.basic1.Dish.menu;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 public class _04Finding {
 
@@ -15,29 +15,33 @@ public class _04Finding {
         System.out.println(isHealthyMenu());
         System.out.println(isHealthyMenu2());
         
-        Optional<Dish> dish = findVegetarianDish();
-        dish.ifPresent(d -> System.out.println(d.getName()));
+        Optional<Dish> optionalDish = findVegetarianDish();
+        System.out.println(optionalDish.isPresent());
+        optionalDish.ifPresent(d -> System.out.println(d.getName()));
     }
 
     //1. anyMatch
     private static boolean isVegetarianFriendlyMenu(){
-        return false;
+        return Dish.menu.stream().anyMatch(Dish::isVegetarian);
     }
+
     //2.allMatch
     private static boolean isHealthyMenu(){
-
-        return false;
+        return Dish.menu.stream().allMatch(dish -> dish.getCalories() <= 800);
     }
 
     //3. noneMatch
     private static boolean isHealthyMenu2(){
-
-        return false;
+        Predicate<Dish> predicate1 = dish -> dish.getCalories() > 800;
+        return Dish.menu.stream().noneMatch(predicate1.or(dish -> dish.getName().contains("g")));
     }
+
     //4. findAny
-    private static Optional<Dish> findVegetarianDish(){
-
-        return null;
+    private static Optional<Dish> findVegetarianDish() {
+        //return Optional.empty();
+        return Dish.menu.stream()
+                .filter(Dish::isVegetarian)
+                .findFirst();
+        //.findAny();
     }
-    
 }
