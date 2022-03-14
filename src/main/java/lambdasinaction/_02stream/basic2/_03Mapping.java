986f6c4fb034efee1441183718948eae2bfb1af4
skskdmsdl <skskdmsdl@gmail.com>
2022-03-14 20:50:54 +0900

    grouping by

--- src/main/java/lambdasinaction/_02stream/basic2/_03Mapping.java
+++ src/main/java/lambdasinaction/_02stream/basic2/_03Mapping.java
@@ -51,6 +51,7 @@ public class _03Mapping {
                 .forEach(System.out::println);
 
         System.out.println("---- flatMap() 시작");
+
         // 3. map(), flatMap() - 중복된 문자 제거한 word 리스트
         stringList.stream()
                 .map(word -> word.split(""))
@@ -78,5 +79,48 @@ public class _03Mapping {
                                 .filter(pair -> (pair[0] + pair[1]) % 3 == 0)
                                 .collect(toList());
         pairs.forEach(pair -> System.out.println("(" + pair[0] + ", " + pair[1] + ")"));
+
+        //----- flatMap 사용하기
+        List<Customer> customers = List.of(new Customer(101, "john", "john@gmail.com", Arrays.asList("397937955", "21654725")),
+                new Customer(102, "smith", "smith@gmail.com", Arrays.asList("89563865", "2487238947")),
+                new Customer(103, "peter", "peter@gmail.com", Arrays.asList("38946328654", "3286487236")),
+                new Customer(104, "kely", "kely@gmail.com", Arrays.asList("389246829364", "948609467")));
+
+        //Stream<R> map(Function<? super T,? extends R> mapper)
+        List<List<String>> phoneList = customers.stream()  //Stream<Customer>
+                .map(customer -> customer.getPhoneNumbers()) //Stream<List<String>>
+                .collect(toList());//List<List<String>>
+        System.out.println("phoneList = " + phoneList);
+
+        //Stream<R> flatMap(Function<? super T,? extends Stream<? extends R>> mapper)
+        List<String> phoneList2 = customers.stream()  //Stream<Customer>
+                .flatMap(customer -> customer.getPhoneNumbers().stream()) //Stream<String>
+                .collect(toList());//List<String>
+        System.out.println("phoneList2 = " + phoneList2);
+
+    }
+
+    static class Customer {
+
+        private int id;
+        private String name;
+        private String email;
+        private List<String> phoneNumbers;
+
+
+        public Customer(int id, String name, String email, List<String> phoneNumbers) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.phoneNumbers = phoneNumbers;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public List<String> getPhoneNumbers() {
+            return phoneNumbers;
+        }
     }
 }
--- src/main/java/lambdasinaction/_02stream/collect/_01GroupingTransactions.java
+++ src/main/java/lambdasinaction/_02stream/collect/_01GroupingTransactions.java
@@ -1,28 +1,39 @@
 package lambdasinaction._02stream.collect;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingDouble;
 
 public class _01GroupingTransactions {
 
     public static List<Transaction> transactions = Arrays.asList( new Transaction(Currency.EUR, 1500.0),
-                                                                  new Transaction(Currency.USD, 2300.0),
-                                                                  new Transaction(Currency.GBP, 9900.0),
-                                                                  new Transaction(Currency.EUR, 1100.0),
-                                                                  new Transaction(Currency.JPY, 7800.0),
-                                                                  new Transaction(Currency.CHF, 6700.0),
-                                                                  new Transaction(Currency.EUR, 5600.0),
-                                                                  new Transaction(Currency.USD, 4500.0),
-                                                                  new Transaction(Currency.CHF, 3400.0),
-                                                                  new Transaction(Currency.GBP, 3200.0),
-                                                                  new Transaction(Currency.USD, 4600.0),
-                                                                  new Transaction(Currency.JPY, 5700.0),
-                                                                  new Transaction(Currency.EUR, 6800.0) );
+            new Transaction(Currency.USD, 2300.0),
+            new Transaction(Currency.GBP, 9900.0),
+            new Transaction(Currency.EUR, 1100.0),
+            new Transaction(Currency.JPY, 7800.0),
+            new Transaction(Currency.CHF, 6700.0),
+            new Transaction(Currency.EUR, 5600.0),
+            new Transaction(Currency.USD, 4500.0),
+            new Transaction(Currency.CHF, 3400.0),
+            new Transaction(Currency.GBP, 3200.0),
+            new Transaction(Currency.USD, 4600.0),
+            new Transaction(Currency.JPY, 5700.0),
+            new Transaction(Currency.EUR, 6800.0) );
     public static void main(String ... args) {
         groupImperatively();
         groupFunctionally();
 
+        //각 트랜잭션을 통화별로 그룹화 한 다음에 해당 통화의 모든 트랜잭션 합계를 계산하시오
+        Map<Currency, Double> currencyDoubleMap = transactions.stream()
+                .collect(groupingBy(Transaction::getCurrency, summingDouble(Transaction::getValue)));
+        System.out.println("currencyDoubleMap = " + currencyDoubleMap);
+
+        //각 트랜잭션을 통화별로 그룹화 한 뒤 각 트랜잭션이 5000 이상일 경우를 구분하여 리스트로 반환하시오.
+        Map<Currency, Map<Boolean, List<Transaction>>> map = transactions.stream()
+                .collect(groupingBy(Transaction::getCurrency, Collectors.partitioningBy(tx -> tx.getValue() >= 5000)));
+        System.out.println("map = " + map);
     }
 
     private static void groupImperatively() {
@@ -31,7 +42,7 @@ public class _01GroupingTransactions {
             Currency currency = transaction.getCurrency();
             List<Transaction> transactionsForCurrency = transactionsByCurrencies.get(currency);
             if (transactionsForCurrency == null) {
-                    transactionsForCurrency = new ArrayList<>();
+                transactionsForCurrency = new ArrayList<>();
                 transactionsByCurrencies.put(currency, transactionsForCurrency);
             }
             transactionsForCurrency.add(transaction);
@@ -42,10 +53,13 @@ public class _01GroupingTransactions {
 
     //Java8 groupingBy 사용
     private static void groupFunctionally() {
+        Map<Currency, List<Transaction>> currencyListMap = transactions.stream()
+                .collect(groupingBy(Transaction::getCurrency));
+        //.collect(groupingBy(tx -> tx.getCurrency()))
 
-
-
+        System.out.println(currencyListMap);
     }
+
 
     public static class Transaction {
         private final Currency currency;
