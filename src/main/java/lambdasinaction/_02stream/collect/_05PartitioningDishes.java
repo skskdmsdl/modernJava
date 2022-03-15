c851da95c6467982cc71b252048206cd485c1219
skskdmsdl <skskdmsdl@gmail.com>
2022-03-15 11:23:11 +0900

    partitioningBy

--- src/main/java/lambdasinaction/_02stream/collect/_05PartitioningDishes.java
+++ src/main/java/lambdasinaction/_02stream/collect/_05PartitioningDishes.java
@@ -1,8 +1,10 @@
 package lambdasinaction._02stream.collect;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
-import static java.util.Comparator.comparingInt;
 import static java.util.stream.Collectors.*;
 import static lambdasinaction._02stream.collect.Dish.menu;
 
@@ -16,17 +18,21 @@ public class _05PartitioningDishes {
 
     //1. 채식요리와 채식요리가 아닌 것으로 분류하기
     private static Map<Boolean, List<Dish>> partitionByVegeterian() {
-        return null;
+        return menu.stream()
+                .collect(partitioningBy(Dish::isVegetarian));
     }
+
     //2. 채식요리와 채식요리가 아닌 것으로 분류한 다음 type별로 그룹핑 하기
     private static Map<Boolean, Map<Dish.Type, List<Dish>>> vegetarianDishesByType() {
-        return null;
+        return menu.stream()
+                .collect(partitioningBy(Dish::isVegetarian, groupingBy(Dish::getType)));
     }
 
     //3. 채식요리와 채식요리가 아닌 것으로 분류한 다음
     // 채식요리 중 칼로리 가장 높은 Dish, 채식요리가 아닌 것 중 칼로리 가장 높은 Dish
     private static Object mostCaloricPartitionedByVegetarian() {
-        return null;
+        return menu.stream()
+                .collect(partitioningBy(Dish::isVegetarian, collectingAndThen(maxBy(Comparator.comparingInt(Dish::getCalories)), Optional::get)));
     }
 }
 
